029b378e1f20f2e7b29d054721bc873eea489952
ethantddavis <ethantddavis@gmail.com>
2022-09-01 21:19:27 -0400

    first implementation of todo list

---
+++ typescript-project/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputFeild from "./components/InputFeild";
+import { Todo } from "./components/model";
+
+
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+    </div>
+  );
+}
+
+export default App;
