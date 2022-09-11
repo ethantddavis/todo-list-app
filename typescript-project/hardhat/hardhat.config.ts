4fde5b1db837dc9eed18b6736777fc345317caa5
ethantddavis <ethantddavis@gmail.com>
2022-09-11 14:46:49 -0400

    deployed contract do goreli

--- typescript-project/hardhat/hardhat.config.ts
+++ typescript-project/hardhat/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.13",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    goreli: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIV_KEY]
+    },
+
+  }
 };
 
 export default config;
